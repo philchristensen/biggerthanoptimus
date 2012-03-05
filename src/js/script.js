309068b428b26d6408f735d834f34dc7b5da17f7
Phil Christensen <pchristensen@freelancersunion.org>
2012-03-04 19:02:58 -0500

    got ajax lookup working

--- src/js/script.js
+++ src/js/script.js
@@ -7,20 +7,28 @@
 	var API_URL = 'https://en.wikipedia.org/w/api.php';
 	
 	function parseArticle(data){
-		return {
-			title: data.parse.title,
-			wordcount: evaluateArticle(data.parse.text['*'])
-		};
-	}
-	
-	function evaluateArticle(html){
+		var articleText = data.parse.text['*'];
 		var wordcount = 0;
-		$(html).find('p').each(function(p){
-			var code = $(p).html();
+		
+		var html = $(articleText);
+		html.filter('p').each(function(){
+			var code = $(this).html();
 			var words = code.replace(/(<([^>]+)>)/ig,"").split(' ');
 			wordcount = wordcount + words.length;
 		});
-		return wordcount;
+		
+		var stripped = $.trim(articleText.replace(/(<([^>]+)>)/ig,""));
+		var excerpt = stripped.substring(0, 100);
+		var redirect = null;
+		if(excerpt.indexOf('REDIRECT') == 0){
+			redirect = stripped.substring(9, excerpt.indexOf('\n'));
+		}
+		return {
+			title: data.parse.title,
+			wordcount: wordcount,
+			excerpt: excerpt,
+			redirect: redirect,
+		};
 	}
 	
 	function getArticle(query, callback){
@@ -28,16 +36,36 @@
 		$.ajax(url, {
 			dataType: 'jsonp',
 			success: function(data, textStatus, jqXHR){
-				displayWinner(parseArticle(data));
+				var article = parseArticle(data);
+				if(article.redirect){
+					getArticle(article.redirect, callback);
+				}
+				else{
+					callback(article);
+				}
 			},
 			error: function(jqXHR, textStatus, errorThrown){
-				displayWinner(textStatus);
+				callback(textStatus);
 			}
 		});
 	}
 	
-	function displayWinner(article){
-		$('#result-box').html(article);
+	function displayWinner(winner, loser){
+		var box = $('#result-box');
+		box.empty();
+		box.append($('<h3>In a Wikipedia matchup</h3>'));
+		box.append($('<h1>' + winner.title + '</h1>'));
+		box.append($('<h3>clocks in at ' + winner.wordcount + ' words, beating out</h3>'));
+		box.append($('<h2>' + loser.title + '</h2>'));
+		box.append($('<h3>with only ' + loser.wordcount + ' words.</h3>'));
+	}
+	
+	function startSpinner(spinner){
+		
+	}
+	
+	function stopSpinner(spinner){
+		
 	}
 	
 	$(document).ready(function(){
@@ -48,10 +76,10 @@
 			getArticle(champion, function(champion){
 				getArticle(challenger, function(challenger){
 					if(champion.wordcount > challenger.wordcount){
-						displayWinner(champion);
+						displayWinner(champion, challenger);
 					}
-					else if(champion.wordcount > challenger.wordcount){
-						displayWinner(challenger);
+					else if(champion.wordcount < challenger.wordcount){
+						displayWinner(challenger, champion);
 					}
 					else{
 						alert('unbelievable: a tie.');
